a32cfd140e19b6feae5a08842c9228dc172109d0
Favreau Guillaume <guillaume.favreau@volvo.com>
2020-03-29 20:10:16 +0200

    Day 09 solved

--- 09/solution.js
+++ 09/solution.js
@@ -6,8 +6,6 @@ const input = fs.readFileSync('input.txt', 'utf-8', (input, err) => {
 });
 
 
-
-
 // table maker
 const tablemaker = (lines, columns, fill) => {
     tempArr = new Array(lines).fill(fill).map(currentRow => {
@@ -15,8 +13,6 @@ const tablemaker = (lines, columns, fill) => {
     })
     return tempArr;
 }
-
-
 
 // INSTRUCTIONS MANAGEMENT
 const instructions = input.split('\r\n');
@@ -33,19 +29,26 @@ const parseInput = instruction => {
 //Table of instructions
 instructionsArray = tablemaker(instructions.length,3,'');
     for (let i = 0; i < instructions.length; i++) {
-        instructionsArray[i][0] = parseInput(instructions[i])[0];
-        instructionsArray[i][1] = parseInput(instructions[i])[1];
-        instructionsArray[i][2] = parseInput(instructions[i])[2];
+        instructionsArray[i] = parseInput(instructions[i]);
     }
 // console.table(instructionsArray);
 //List of cities (unique)
-const cities = [...new Set(instructionsArray.map(instruction => instruction[0]))];
+const citiesAll = array =>{
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(array[i][0]);
+        result.push(array[i][1]);
+    }
+    return result;
+};
+const cities = [...new Set(citiesAll(instructionsArray).map(instruction => instruction))];
 
-// Function to find all arrays with specific value in an array of arrays
-const checkValue = (arrayOfarray, value) => {
-    let found = arrayOfarray.filter( array => array.includes(value));
-    return found;
+
+// Function to find all arrays with specific value in instructionsArray
+const checkValue = (arrayOfarrays, value) => {
+    return arrayOfarrays.filter( array => array.includes(value));
 }
+
 
 //Function to find minimum in last column
 const minimum = (arrayOfarrays) => {
@@ -53,12 +56,40 @@ const minimum = (arrayOfarrays) => {
     let min = arrayOfarrays[0][l-1];
     for (let j = 0; j < 2; j++) {
         for (let i = 0; i < arrayOfarrays.length; i++) {
-            if (min > arrayOfarrays[i][l-1]) {
-                min = arrayOfarrays[i][l-1];
+            if (min > parseInt(arrayOfarrays[i][l-1])) {
+                min = parseInt(arrayOfarrays[i][l-1]);
             }
         }
     }
     return min;
+}
+
+//Function to find maximun in last column
+const maximum = (arrayOfarrays) => {
+    let l = arrayOfarrays[0].length;
+    let max = arrayOfarrays[0][l-1];
+    for (let j = 0; j < 2; j++) {
+        for (let i = 0; i < arrayOfarrays.length; i++) {
+            if (max < parseInt(arrayOfarrays[i][l-1])) {
+                max = parseInt(arrayOfarrays[i][l-1]);
+            }
+        }
+    }
+    return max;
+}
+
+//Function to compare arrays
+const areArraysEqual = (array1, array2) => {
+    if(array1.length === array2.length) {
+        for (let i = 0; i < array1.length; i++) {
+            if( array1[i] !== array2[i]) {
+                return false
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
@@ -87,15 +118,21 @@ const permutator = (inputArr) => {
    return result;
   }
 
+const CitiesCombinations = permutator(cities);
+
+
 const calculator = arrayOftrips => {
     let distance =0;
     for (let i = 0; i < arrayOftrips.length; i++) {
         let total =0;
-        for (let j = 0; j < arrayOftrips[i].length-1; j++) {
-            ArraysWithCurrValue = checkValue(instructionsArray,arrayOftrips[i][j]);
-            CurrPair = [arrayOftrips[i][j] , arrayOftrips[i][j+1]];
-            for (let k = 0; k < ArraysWithCurrValue.length; k++) {
-                if (([ArraysWithCurrValue[k][0] , ArraysWithCurrValue[k][1]] = CurrPair) || ([ArraysWithCurrValue[k][1] , ArraysWithCurrValue[k][0]] = CurrPair)) {
+        for (let j = 0; j < arrayOftrips[i].length-1 ; j++) {
+            CurrValue = arrayOftrips[i][j]; // Current city in current trip (line)
+            ArraysWithCurrValue =  checkValue(instructionsArray , CurrValue); //Look for all arrays with the current city in input
+            CurrPair = [arrayOftrips[i][j] , arrayOftrips[i][j+1]]; //creates array of current city & next city
+            for (let k = 0; k < ArraysWithCurrValue.length; k++) { // check all the arrays with current city and find the array with next city
+                testArray = [ ArraysWithCurrValue[k][0] , ArraysWithCurrValue[k][1] ];
+                testArray2 = [ ArraysWithCurrValue[k][1] , ArraysWithCurrValue[k][0] ];
+                if (areArraysEqual(CurrPair,testArray) || areArraysEqual(CurrPair , testArray2)) { //
                     distance = parseInt(ArraysWithCurrValue[k][2]);
                 } 
             }
@@ -104,18 +141,11 @@ const calculator = arrayOftrips => {
         arrayOftrips[i].push(total);
     }
     // console.table(arrayOftrips);
-    console.log(minimum(arrayOftrips));
+    console.log('minimum:');console.log(minimum(arrayOftrips));
+    console.log('maximum:');console.log(maximum(arrayOftrips));
 }
 
-let combinations = permutator(cities);
-calculator(combinations);
 
-// console.table(checkValue(instructionsArray,'Snowdin'));
-
-// console.table(combinations);
+calculator(CitiesCombinations);
 
 
-//TEST
-// arr = [['a','z'], ['a','a'], ['c','w']];
-// console.log(checkValue(arr,'a'));
- 
